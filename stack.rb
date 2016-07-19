5a2ad061095d4d41e2b404107177ed09811215e7
Danny Bravman <dbravman@gmail.com>
2016-07-19 15:34:47 -0500

    tests

--- stack.rb
+++ stack.rb
@@ -1,26 +1,23 @@
 require_relative "linked_list"
 
-class Stack
-  def initialize
-    @stack = LinkedList.new
-  end
+class Stack < LinkedList
 
   def push(element)
-    @stack.insert_last(element)
+    insert_last(element)
   end
 
   def pop
     element = top
-    @stack.remove_last
+    remove_last
     element
   end
 
   def top
-    return nil if @stack.empty?
-    @stack.get(@stack.size-1)
+    return nil if empty?
+    get(size-1)
   end
 
   def empty?
-    @stack.size == 0
+    size == 0
   end
 end
