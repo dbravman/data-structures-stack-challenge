98e6bbf3227ea3511846cee0e433780f7100e952
Danny Bravman <dbravman@gmail.com>
2016-07-19 17:49:50 -0500

    use quotes to handle argv exceptions for * and /

--- calculate.rb
+++ calculate.rb
@@ -1,4 +1,8 @@
 require_relative "rpn_calculator"
 
-input = ARGV.split(" ")
-puts RPNCaluclator.new.calculate(input)
+
+if ARGV.length != 1
+  puts "Enclose input within quotes"
+else 
+  puts RPNCalculator.new.calculate(ARGV[0].split(" "))
+end
--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,6 +1,8 @@
 require_relative "stack"
 
 class RPNCalculator
+  attr_reader :values
+
   def initialize
     @values = Stack.new
   end
@@ -9,21 +11,23 @@ class RPNCalculator
     input.each do |token|
       if token == "+" || token == "-" || token == "*" || token == "/"
         raise "too few values" if @values.size < 2
+        num2 = @values.pop
+        num1 = @values.pop
         case token
-          when "+" then @values.push(@values.pop + @values.pop) 
-          when "-" then @values.push(@values.pop - @values.pop)
-          when "*" then @values.push(@values.pop * @values.pop)
-          when "/" then @values.push(@values.pop / @values.pop)
+          when "+" then @values.push(num1 + num2) 
+          when "-" then @values.push(num1 - num2)
+          when "*" then @values.push(num1 * num2)
+          when "/" then @values.push(num1 / num2)
         end
       else #token is a number
         @values.push(token.to_f)
       end
     end
-    
+  
     if @values.size == 1
       @values.top
     else
-      raiase "too many values"
+      raise "too many values"
     end
   end
 end
