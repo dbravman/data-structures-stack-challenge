77fb016f650eb0fc0839856a376fcfaed39efcf9
Danny Bravman <dbravman@gmail.com>
2016-07-19 15:12:09 -0500

    stack methods

--- stack.rb
+++ stack.rb
@@ -1,3 +1,26 @@
-class Stack
+require_relative "linked_list"
 
+class Stack
+  def initialize
+    @stack = LinkedList.new
+  end
+
+  def push(element)
+    @stack.insert_last(element)
+  end
+
+  def pop
+    element = top
+    @stack.remove_last
+    element
+  end
+
+  def top
+    return nil if @stack.empty?
+    @stack.get(@stack.size-1)
+  end
+
+  def empty?
+    @stack.size == 0
+  end
 end
